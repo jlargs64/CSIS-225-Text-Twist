e89112367080fdf13b9acf3253b8a6740f12c73c
jlargs64 <justinlargo7@gmail.com>
2019-03-17 18:12:46 -0400

    Buttons can now read in input from files

--- TextTwist.java
+++ TextTwist.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.awt.*;
@@ -10,13 +11,16 @@ import javax.swing.*;
  * @author Justin Largo
  * @version 1.0
  */
-public class TextTwist extends JPanel implements MouseListener {
+public class TextTwist extends JPanel implements MouseListener, ActionListener {
 
     // Instance Variables
     private static final long serialVersionUID = 9136266265671208067L;
     private int width, height, x, y;
-    private String currentWord = "";
-    private JButton board1, board2, board3;
+    private ArrayList<String> gameWords = new ArrayList<>();
+    private File board1, board2, board3;
+    private JButton buttonBoard1, buttonBoard2, buttonBoard3;
+    private Scanner boardScanner;
+
     public TextTwist() {
 
         setPreferredSize(new Dimension(800, 600));
@@ -26,28 +30,81 @@ public class TextTwist extends JPanel implements MouseListener {
         setBackground(Color.BLACK);
         setFocusable(true);
 
-        x = width / 2;
-        y = height / 2;
+        // Setting the files to have game data
+        board1 = new File("game1.txt");
+        board2 = new File("game2.txt");
+        board3 = new File("game3.txt");
+
+        // Adding Game board buttons
+        buttonBoard1 = new JButton("Board 1");
+        buttonBoard1.setVerticalTextPosition(AbstractButton.CENTER);
+        buttonBoard1.setHorizontalTextPosition(AbstractButton.CENTER);
+
+        buttonBoard2 = new JButton("Board 2");
+        buttonBoard2.setVerticalTextPosition(AbstractButton.CENTER);
+        buttonBoard2.setHorizontalTextPosition(AbstractButton.CENTER);
+
+        buttonBoard3 = new JButton("Board 3");
+        buttonBoard3.setVerticalTextPosition(AbstractButton.CENTER);
+        buttonBoard3.setHorizontalTextPosition(AbstractButton.CENTER);
 
         // Add interface listeners
         addMouseListener(this);
+        buttonBoard1.addActionListener(this);
+        buttonBoard2.addActionListener(this);
+        buttonBoard3.addActionListener(this);
+
+        // Add buttons to container
+        add(buttonBoard1);
+        add(buttonBoard2);
+        add(buttonBoard3);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        //Create title text
+        // Create title text
         Font titleFont = new Font("Monospace", Font.BOLD, 32);
         g.setFont(titleFont);
         g.setColor(Color.WHITE);
-        g.drawString("Text Twist", width-200, 100);
+        g.drawString("Text Twist", width - 200, 100);
     }
 
-    public void keyPressed(KeyEvent e) {
-    }
+    public void actionPerformed(ActionEvent e) {
 
-    public void keyReleased(KeyEvent e) {
+        try {
+
+            /*
+             * Selecting which gameboard to play with then read in words from a file for the
+             * selected board.
+             */
+            if (buttonBoard1.getModel().isPressed()) {
+
+                boardScanner = new Scanner(board1);
+                while (boardScanner.hasNext()) {
+                    gameWords.add(boardScanner.nextLine());
+                }
+            } else if (buttonBoard2.getModel().isPressed()) {
+
+                boardScanner = new Scanner(board2);
+                while (boardScanner.hasNext()) {
+                    gameWords.add(boardScanner.nextLine());
+                }
+            } else {
+
+                boardScanner = new Scanner(board3);
+                while (boardScanner.hasNext()) {
+                    gameWords.add(boardScanner.nextLine());
+                }
+            }
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        } finally {
+
+            boardScanner.close();
+        }
     }
 
     public void mouseEntered(MouseEvent e) {
@@ -82,7 +139,7 @@ public class TextTwist extends JPanel implements MouseListener {
                 TextTwist panel = new TextTwist();
                 frame.getContentPane().add(panel);
 
-                //Making sure the game scales well
+                // Making sure the game scales well
                 frame.setResizable(false);
                 // Display the window.
                 frame.pack();
