b57a7af805da278922d014929a12540649e0bd6e
jlargs64 <justinlargo7@gmail.com>
2019-03-18 14:38:29 -0400

    Proper button clicking

--- TextTwist.java
+++ TextTwist.java
@@ -20,6 +20,7 @@ public class TextTwist extends JPanel implements MouseListener, ActionListener {
     private ArrayList<String> gameWords = new ArrayList<>();
     private File board1, board2, board3, board4;
     private JButton buttonBoard1, buttonBoard2, buttonBoard3, buttonBoard4;
+    private JButton helpButton, exitButton;
     private Scanner boardScanner;
     private int score;
 
@@ -97,34 +98,45 @@ public class TextTwist extends JPanel implements MouseListener, ActionListener {
             buttonBoard2 = new JButton("Board 2");
             buttonBoard2.setVerticalTextPosition(AbstractButton.CENTER);
             buttonBoard2.setHorizontalTextPosition(AbstractButton.CENTER);
-            buttonBoard2.setBounds(100, 200, 300, 100);
+            buttonBoard2.setBounds(100, 160, 300, 100);
 
             buttonBoard3 = new JButton("Board 3");
             buttonBoard3.setVerticalTextPosition(AbstractButton.CENTER);
             buttonBoard3.setHorizontalTextPosition(AbstractButton.CENTER);
-            buttonBoard3.setBounds(100, 300, 300, 100);
+            buttonBoard3.setBounds(100, 270, 300, 100);
 
             buttonBoard4 = new JButton("Board 4");
             buttonBoard4.setVerticalTextPosition(AbstractButton.CENTER);
             buttonBoard4.setHorizontalTextPosition(AbstractButton.CENTER);
-            buttonBoard4.setBounds(100, 350, 300, 100);
+            buttonBoard4.setBounds(100, 380, 300, 100);
+
+            helpButton = new JButton("Help");
+            helpButton.setVerticalTextPosition(AbstractButton.CENTER);
+            helpButton.setHorizontalTextPosition(AbstractButton.CENTER);
+            helpButton.setBounds(450, 380, 300, 100);
 
             // Add interface listeners
             addMouseListener(this);
             buttonBoard1.addActionListener(this);
             buttonBoard2.addActionListener(this);
             buttonBoard3.addActionListener(this);
+            buttonBoard4.addActionListener(this);
+            helpButton.addActionListener(this);
 
             // Make buttons visible
             buttonBoard1.setVisible(true);
             buttonBoard2.setVisible(true);
             buttonBoard3.setVisible(true);
             buttonBoard4.setVisible(true);
+            helpButton.setVisible(true);
 
             // Add buttons to container
             add(buttonBoard1);
             add(buttonBoard2);
             add(buttonBoard3);
+            add(buttonBoard4);
+            add(helpButton);
+
             break;
 
         case GAME_MENU:
@@ -134,6 +146,7 @@ public class TextTwist extends JPanel implements MouseListener, ActionListener {
             buttonBoard2.setVisible(false);
             buttonBoard3.setVisible(false);
             buttonBoard4.setVisible(false);
+            helpButton.setVisible(false);
 
             // Draw are useful game text
             g.drawString("SCORE: " + score, width - 200, height - 100);
@@ -141,6 +154,14 @@ public class TextTwist extends JPanel implements MouseListener, ActionListener {
             break;
 
         case HELP_MENU:
+
+            // Disable buttons
+            buttonBoard1.setVisible(false);
+            buttonBoard2.setVisible(false);
+            buttonBoard3.setVisible(false);
+            buttonBoard4.setVisible(false);
+            helpButton.setVisible(false);
+
             break;
         }
     }
@@ -148,28 +169,34 @@ public class TextTwist extends JPanel implements MouseListener, ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         try {
+            // Enter the help screen
+            if (e.getSource() == helpButton) {
 
+                // Set the state to Help Menu
+                currentState = GameState.values()[2];
+                repaint();
+            }
             // Selecting which gameboard to play with then read in words
             // from a file for the selected board.
-            if (buttonBoard1.getModel().isPressed()) {
+            if (e.getSource() == buttonBoard1) {
 
                 boardScanner = new Scanner(board1);
                 while (boardScanner.hasNext()) {
                     gameWords.add(boardScanner.nextLine());
                 }
-            } else if (buttonBoard2.getModel().isPressed()) {
+            } else if (e.getSource() == buttonBoard2) {
 
                 boardScanner = new Scanner(board2);
                 while (boardScanner.hasNext()) {
                     gameWords.add(boardScanner.nextLine());
                 }
-            } else if (buttonBoard3.getModel().isPressed()) {
+            } else if (e.getSource() == buttonBoard3) {
 
                 boardScanner = new Scanner(board3);
                 while (boardScanner.hasNext()) {
                     gameWords.add(boardScanner.nextLine());
                 }
-            } else {
+            } else if (e.getSource() == buttonBoard4) {
 
                 boardScanner = new Scanner(board4);
                 while (boardScanner.hasNext()) {
